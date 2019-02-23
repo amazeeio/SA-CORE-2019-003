6e31b4c3d8495fe2189b5b32eb1976b3f61a30c4
Schnitzel <michael@amazee.com>
2019-02-23 01:46:19 -0500

    no need for token and server

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -eu -o pipefail
 
-OC="oc --insecure-skip-tls-verify --token=${OPENSHIFT_TOKEN} --server=${OPENSHIFT_CONSOLE}"
+OC="oc"
 
 oc get projects --no-headers -o custom-columns=name:.metadata.name | while read project; do
   PROJECT=$project
