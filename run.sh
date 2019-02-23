8bd2a6dbf92f90af1733706c5f839958aa237044
Schnitzel <michael@amazee.com>
2019-02-22 23:12:41 -0500

    better error

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu -o pipefail
+
 OC="oc --insecure-skip-tls-verify --token=${OPENSHIFT_TOKEN} --server=${OPENSHIFT_CONSOLE}"
 
 oc get projects --no-headers -o custom-columns=name:.metadata.name | while read project; do
