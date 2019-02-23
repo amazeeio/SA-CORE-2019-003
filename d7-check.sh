d57792ff50d2d0ee3967160254ca668f052f88e7
Brandon Williams <brandon@amazeelabs.com>
2019-02-22 22:42:25 -0600

    Add script to disable restws module for d7

---
+++ d7-check.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+echo 'Drupal SA-CORE-2019-003 auto disable script for 7x.x'
+
+d7Status="$(drush status --fields=drupal-version)"
+d7RestwsInfo="$(drush pm-info --fields=status,version restws)"
+d7ServicesInfo="$(drush pm-info --fields=status,version services)"
+
+# Only need to disable modules if services or rest are installed and vulnerable
+if [[ $d7RestwsInfo == *enabled* ]] || [[ $d7Servicesinfo == *enabled* ]]
+then
+  echo 'services or restws enabled, checking versions'
+
+  # Services itself is not vulnerable, but if it's enabled, we need to check
+  # contrib modules
+  if [[ $d7Servicesinfo == *enabled* ]]
+  then
+    echo 'services enabled, checking contrib modules'
+  fi
+
+  # RestWS should be disabled if vulnerable, but if fixed we still need to check
+  # contrib modules
+  if [[ $d7RestwsInfo != *7.x-2.8* ]]
+  then
+    echo 'restws is not version 7.x-2.8, disabling'
+    drush pm-disable -y restws
+  else
+    echo 'restws is version 7.x-2.8, checking contrib modules'
+  fi
+
+else
+  echo 'services and restws are disabled, doing nothing'
+fi
