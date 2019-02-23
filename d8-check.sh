a9931800c62654d5c817831e1a604dbd58d328aa
Brandon Williams <brandon@amazeelabs.com>
2019-02-22 23:21:14 -0600

    Add d8 contrib checking/uninstalling, but commented out

--- d8-check.sh
+++ d8-check.sh
@@ -6,6 +6,43 @@ d8Status="$(drush status --fields=drupal-version 2> /dev/null)"
 d8RestInfo="$(drush pm-info --fields=status,version rest 2> /dev/null)"
 d8JsonapiInfo="$(drush pm-info --fields=status,version jsonapi 2> /dev/null)"
 
+function uninstallingContrib() {
+  d8fontawesomeInfo="$(drush pm-info --fields=version fontawesome 2> /dev/null)"
+  if [[ $d8fontawesomeInfo != *8.x-2.2* ]]
+  then
+    echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: fontawesome is not version 8.x-1.6, uninstalling"
+    drush pm-uninstalling -y fontawesome
+  fi
+
+  d8tmgmtInfo="$(drush pm-info --fields=version tmgmt 2> /dev/null)"
+  if [[ $d8tmgmtInfo != *8.x-1.7* ]]
+  then
+    echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: tmgmt is not version 8.x-1.7, uninstalling"
+    drush pm-uninstalling -y tmgmt
+  fi
+
+  d8paragraphsInfo="$(drush pm-info --fields=version paragraphs 2> /dev/null)"
+  if [[ $d8paragraphsInfo != *8.x-1.6* ]]
+  then
+    echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: paragraphs is not version 8.x-1.6, uninstalling"
+    drush pm-uninstalling -y paragraphs
+  fi
+
+  d8videoInfo="$(drush pm-info --fields=version video 2> /dev/null)"
+  if [[ $d8videoInfo != *8.x-1.4* ]]
+  then
+    echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: video is not version 8.x-1.4, uninstalling"
+    drush pm-uninstalling -y video
+  fi
+
+  d8metatagInfo="$(drush pm-info --fields=version metatag 2> /dev/null)"
+  if [[ $d8metatagInfo != *8.x-1.8* ]]
+  then
+    echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: metatag is not version 8.x-1.8, uninstalling"
+    drush pm-uninstalling -y metatag
+  fi
+}
+
 # Check for vulnerable rest module
 if [[ $d8RestInfo == *enabled* ]]
 then
@@ -14,7 +51,8 @@ then
     echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: rest module is not version 8.6.10, uninstalling";
     drush pm-uninstall -y rest
   else
-    echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: rest module is 8.6.10, doing nothing";
+    echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: rest module is 8.6.10, checking contrib";
+    uninstallingContrib
   fi
 else
   echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: rest is not enabled";
@@ -37,8 +75,9 @@ then
       echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: jsonapi module is not version 8.x-2.3 or 8.x-1.25, uninstalling";
       drush pm-uninstall -y jsonapi
     else
-      echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: jsonapi module is up to date, doing nothing";
+      echo "$LAGOON_PROJECT-$LAGOON_GIT_SAFE_BRANCH: jsonapi module is up to date, checking contrib";
       echo $d8JsonapiInfo;
+      uninstallingContrib
     fi
 
   fi
