c45adb9ec4ce4e2f5ce7282bb897a45babde2a96
Schnitzel <michael@amazee.com>
2019-02-23 01:02:26 -0500

    legacy checks

--- run-legacy.sh
+++ run-legacy.sh
@@ -3,10 +3,11 @@
 
 set -eu -o pipefail
 
+check=$(<d8-check.sh)
 for dir in /var/www/*
 do
   user=${dir#/var/www/}
   if id -u $user &> /dev/null; then
-    su $user bash -c "cd \$AMAZEEIO_WEBROOT; drush status"
+    su $user bash -c "cd \$AMAZEEIO_WEBROOT; $check"
   fi
 done
